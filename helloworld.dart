253be1dfcb3a7214aae35438e7924f0d45fcc18e
Dipesh Dhungana <dipesh.077bct034@acem.edu.np>
2025-05-16 12:28:50 +0545

    details on integer variable

--- helloworld.dart
+++ helloworld.dart
@@ -1,5 +1,13 @@
-void main()
+/*
+void main()//main function (returning null value)
 {
-    print("Hello World");
+    print("Hello World");//print syntax is used for printing
     print("My First Program in Dart");
+}
+*/
+
+int main()
+{
+    print("Hello world from int main function");
+    return 1;//since the return type of the main function is int we are returning the value 1 i.e integer value
 }
---
+++ variables/integer.dart
@@ -0,0 +1,25 @@
+late int dipesh;//Method 4 // providing the value later
+
+void main()
+{
+    int x; //Method 1
+    x=1;
+    print('x=$x');
+
+    var y=2; //Method 2
+    print('y=$y');
+
+    int z=3; //Method 3
+    print('z=$z');
+
+    dipesh=7; //Method 4
+    print('dipesh=$dipesh');
+
+    const int maxSpeedlimit=50; //using constant with integer variable
+    print('maxSpeedlimit= $maxSpeedlimit');
+
+    final int maxScore=80; //using final with int 
+    print('maxScore= $maxScore');
+    final maxValue=100; //ommiting the datatype
+    print ('maxValue= $maxValue');
+}
