29229465fe3b29403e45b50ccbce4f904bce6d92
Dipesh Dhungana <dipesh.077bct034@acem.edu.np>
2025-05-19 10:28:51 +0545

    string interpolation and basic calculation

---
+++ basic_calculation.dart
@@ -0,0 +1,16 @@
+void main() {
+  int num1 = 10; //number 1 declaration
+  int num2 = 20; //number 2 declaration
+
+  //calculation
+  int sum = num1 + num2;
+  int diff = num1 - num2;
+  int mul = num1 * num2;
+  double div = num1 / num2; //output might be with decimal
+
+  //display output
+  print("The sum of $num1 and $num2 is $sum \n");
+  print("The difference of $num1 and $num2 is $sum \n");
+  print("The multiple of $num1 and $num2 is $sum \n");
+  print("The division of $num1 and $num2 is $sum \n");
+}
---
+++ string_interpolation.dart
@@ -0,0 +1,5 @@
+void main() {
+  var firstName = 'Dipesh';
+  var lastName = 'Dhungana';
+  print("My name is $firstName $lastName");
+}
