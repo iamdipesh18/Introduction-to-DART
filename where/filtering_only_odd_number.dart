482ea9ffba9f137d5925ee0e29d41b73ced537bb
Dipesh Dhungana <dipesh.077bct034@acem.edu.np>
2025-05-22 15:06:56 +0545

    where in dart

---
+++ where/filtering_only_odd_number.dart
@@ -0,0 +1,14 @@
+/*
+Iterable<E> where(
+bool test(
+E element
+)
+)
+*/
+void main() {
+  List<int> numbers = [2, 4, 6, 8, 10, 11, 12, 13, 14];
+
+  List<int> oddNumbers;
+  oddNumbers = numbers.where((number) => number.isOdd).toList();
+  print(oddNumbers);
+}
