c77a565e0b984a138dfdfdea5a9d32cebea864fd
Dipesh Dhungana <dipesh.077bct034@acem.edu.np>
2025-05-23 12:24:52 +0545

    file handling in dart

---
+++ file/reading_from_specific_directory.dart
@@ -0,0 +1,11 @@
+// dart program to read from file
+import 'dart:io';
+
+void main() {
+  // open file
+  File file = File('C:\\Users\\test.txt');
+  // read file
+  String contents = file.readAsStringSync();
+  // print file
+  print(contents);
+}
